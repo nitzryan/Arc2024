ff0074844833fdc97d6c06aa52eb064e72e6cca0
nitzryan <nitzryan@gmail.com>
2024-08-30 08:48:43 -0500

    Checked Test Comparison Code

--- test_runner.py
+++ test_runner.py
@@ -4,14 +4,14 @@ import torch
 from typing import List, Tuple, Dict, Optional
 
 class Algorithm:
-    def __init__():
+    def __init__(self):
         pass
     
     def Solve_Puzzle(self,
                      train_inputs : List[torch.tensor], 
                     train_outputs : List[torch.tensor], 
                     test_inputs : List[torch.tensor]) -> Optional[torch.Tensor] :
-        return None
+        return torch.tensor([[2,0,2],[0,0,0],[0,0,0]])
 
 def Check_Solution(file, solution):
     with open(file) as solution_file:
@@ -20,21 +20,26 @@ def Check_Solution(file, solution):
 def Test_Dispatcher(train_inputs : List[torch.tensor], 
                     train_outputs : List[torch.tensor], 
                     test_inputs : List[torch.tensor],
-                    solution : torch.tensor,
+                    solutions : torch.tensor,
                     algorithms : List[Algorithm]) -> bool :     
     success_found = False
     failure_found = False
     for algorithm in algorithms:
         test_solution = algorithm.Solve_Puzzle(train_inputs, train_outputs, test_inputs)
         if test_solution is not None:
-            if torch.equal(solution, test_solution):
-                success_found = True
-            else:
-                success_found = False
+            algo_found_solution = False
+            for i in range(solutions.size(0)):
+                solution = solutions[i]
+                if torch.equal(solution, test_solution):
+                    success_found = True
+                    algo_found_solution = True
+            if not algo_found_solution:
+                failure_found = True
+                
                 
     return success_found and not failure_found
 
-
+a = Algorithm()
 with open("data/arc-agi_training_challenges.json") as easy_challenges:
     with open("data/arc-agi_training_solutions.json") as easy_solutions:
         challenges_json = json.load(easy_challenges)
@@ -54,14 +59,14 @@ with open("data/arc-agi_training_challenges.json") as easy_challenges:
                 for t in challenges_json[problem]["test"]:
                     test_inputs.append(torch.tensor(t["input"]))
                     
-                solution = torch.tensor(solutions_json[problem])
-                solution_found = Test_Dispatcher(train_inputs, train_outputs, test_inputs, solution, [])
+                solutions = torch.tensor(solutions_json[problem])
+                solution_found = Test_Dispatcher(train_inputs, train_outputs, test_inputs, solutions, [a])
                 if solution_found:
                     successful_puzzles += 1
                 else:
                     failed_puzzles += 1
-            except:
-                pass
+            except Exception as e:
+                print(e)
                 
         print(f"Successfully completed {successful_puzzles} out of {successful_puzzles + failed_puzzles} easy puzzles")
         print(f"Success rate of {successful_puzzles / (successful_puzzles + failed_puzzles) * 100:.1f}%")
