3c56811ea890f22f52940865aec2c8f7daab7608
nitzryan <nitzryan@gmail.com>
2024-08-31 15:23:06 -0500

    Added per-puzzle tracker to see overall results

--- database/database.py
+++ database/database.py
@@ -29,6 +29,15 @@ def Generate_DB(filename : str) -> None:
                         PRIMARY KEY("PuzzleId","TestId")
                    );
                    ''')
+    cursor.execute('''
+                   CREATE TABLE "PuzzleResults" (
+                       "PuzzleId" TEXT,
+                       "AnyPassed" INTEGER,
+                       "AnyWrong" INTEGER,
+                       "OnlyPassed" INTEGER,
+                       PRIMARY KEY("PuzzleId")
+                   );
+                   ''')
     _db.commit()
     
 def Log_Test_Result(puzzle_id : str,
@@ -43,6 +52,14 @@ def Log_Test_Result(puzzle_id : str,
     try:
         cursor = _db.cursor()
         cursor.execute("INSERT INTO TestResults VALUES(?,?,?,?,?,?,?,?)", (puzzle_id, test_id, test_passed, test_wrong, test_assumption, test_validation, test_exception, algorithm_exception))
+        _db.commit()
+    except Exception as e:
+        print(e)
+        
+def Log_Puzzle_Result(puzzle_id : str, test_passed : bool, test_failed : bool) -> None :
+    try:
+        cursor = _db.cursor()
+        cursor.execute("INSERT INTO PuzzleResults VALUES(?,?,?,?)", (puzzle_id, test_passed, test_failed, test_passed and not test_failed))
         _db.commit()
     except Exception as e:
         print(e)
--- test_runner.py
+++ test_runner.py
@@ -1,54 +1,9 @@
 from tqdm import tqdm
 import json
 import torch
-from typing import List, Tuple, Dict, Optional
-from algorithms.algorithm import Algorithm
 from algorithms.convolutional.one_shot import One_Shot_Convolutional_Algorithm
-from database.database import Generate_DB, Log_Test_Result
-
-def Check_Solution(file, solution):
-    with open(file) as solution_file:
-        solution = json.loads(solution_file)
-                
-def Test_Dispatcher(train_inputs : List[torch.tensor], 
-                    train_outputs : List[torch.tensor], 
-                    test_inputs : List[torch.tensor],
-                    solutions : torch.tensor,
-                    algorithms : List[Algorithm],
-                    puzzle_id : str) -> bool :     
-    success_found = False
-    failure_found = False
-    for algorithm in algorithms:
-        try:
-            test_solutions, test_info = algorithm.Solve_Puzzle(train_inputs, train_outputs, test_inputs)
-            if test_solutions is not None and len(test_solutions) == len(solutions):
-                algo_failed_test = False
-                algo_successful_test = False
-                for i in range(solutions.size(0)):
-                    solution = solutions[i]
-                    test_solution = test_solutions[i]
-                    if not torch.equal(solution, test_solution):
-                        algo_failed_test = True
-                    else:
-                        algo_successful_test = True
-                if algo_failed_test:
-                    failure_found = True
-                    Log_Test_Result(puzzle_id, algorithm.id, False, True, False, False, None, None)
-                elif algo_successful_test:
-                    success_found = True
-                    Log_Test_Result(puzzle_id, algorithm.id, True, False, False, False, None, None)
-            else: # No results, log why
-                if test_info == "Validation_Step":
-                    Log_Test_Result(puzzle_id, algorithm.id, False, False, False, True, None, None)
-                elif test_info == "Assumption_Step":
-                    Log_Test_Result(puzzle_id, algorithm.id, False, False, True, False, None, None)
-                else:
-                    Log_Test_Result(puzzle_id, algorithm.id, False, False, False, False, None, test_info)
-        except Exception as e:
-            Log_Test_Result(puzzle_id, algorithm.id, False, False, False, False, e.__str__(), None)
-                
-                
-    return success_found and not failure_found
+from database.database import Generate_DB
+from test_dispatcher import Test_Dispatcher
 
 torch.set_printoptions(linewidth=1000, edgeitems=20)
 Generate_DB("database/test_results.db")
